53b35fc8802e94bcf873e10b942fce609903ad7c
bre-sew <breannaesewell@gmail.com>
2021-10-16 13:21:12 -0700

    rendered heat map, added leaflet scripts to layout.html

--- static/heatmap.js
+++ static/heatmap.js
@@ -1,4 +1,39 @@
-// Load Data
-d3.json(`/inactive/fires`).then(function(data){
-    console.log(data)
+// set the map center
+var heatmap = L.map("heatmap", {
+    center: [38.573, -121,494],
+    zoom: 5
+});
+
+// add the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(heatmap);
+
+
+// load fire data
+d3.json(`/inactive/fires`).then(function(fires){
+    
+    console.log(fires);
+
+    // 1 acre = 43,560 square feet
+    // 1 foot = 0.3048 meter 
+    // radius = sqrt(area/pi)
+    // var fireRadius = Math.sqrt(fires.AcresBurned/Math.PI)
+
+    var heatArray = [];
+
+    for (var i = 0; i < fires.length; i++) {
+        var location = [fires[i].Latitude, fires[i].Latitude];
+
+        if (location) {
+            heatArray.push(location);
+
+        }
+    }
+
+    var heat = L.heatLayer(heatArray, {
+        radius: fires.map(fire => fire.AcresBurned),
+        blur: 35
+    }).addTo(heatmap);
+
 })

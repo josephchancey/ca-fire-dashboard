d578c820230a911b1c47c4f684ecb9fadceb646f
bre-sew <breannaesewell@gmail.com>
2021-10-18 20:36:02 -0700

    renamed js file to firemap.js

--- static/firemap.js
+++ static/firemap.js
@@ -1,5 +1,5 @@
 // set the map center
-var heatmap = L.map("heatmap", {
+var firemap = L.map("firemap", {
     center: [38.573, -121,494],
     zoom: 5
 });
@@ -7,7 +7,7 @@ var heatmap = L.map("heatmap", {
 // add the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(heatmap);
+}).addTo(firemap);
 
 
 // load fire data
@@ -28,6 +28,6 @@ d3.json(`/inactive/fires`).then(function(fires){
     var heat = L.heatLayer(heatArray, {
         radius: 25,
         blur: 35
-    }).addTo(heatmap);
+    }).addTo(firemap);
 
 })
